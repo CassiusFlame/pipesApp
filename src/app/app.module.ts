3d2e16082ccfca3af4a3a6aff7cbe30681a428c7
CassiusFlame <ibacetanicolas231@gmail.com>
2021-12-13 15:41:38 -0300

    Enter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './ventas/ventas.module';
+
+import localEsAr from '@angular/common/locales/es-AR';
+import localFr from '@angular/common/locales/fr';
+import {registerLocaleData} from '@angular/common';
+registerLocaleData(localEsAr);
+registerLocaleData(localFr);
 
 @NgModule({
   declarations: [
@@ -9,8 +18,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    PrimeNgModule,
+    SharedModule,
+    AppRouterModule,
+    VentasModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-AR'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ButtonModule} from 'primeng/button';
 import {CardModule} from 'primeng/card';
-
+import {FieldsetModule} from 'primeng/fieldset'
+import {MenubarModule} from 'primeng/menubar'
 
 @NgModule({
   declarations: [],
   exports:[
     CardModule,
-    ButtonModule
+    ButtonModule,
+    MenubarModule,
+    FieldsetModule
   ],
   imports: [
     CommonModule,
